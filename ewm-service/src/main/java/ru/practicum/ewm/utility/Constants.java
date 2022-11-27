39a977c6c47c7d41ddce9c44607f55dcb90dfc39
Alexander <71982238+AlSl94@users.noreply.github.com>
2022-11-27 21:09:35 +0300

    feat: fixed final in TIME_FORMATTER

--- ewm-service/src/main/java/ru/practicum/ewm/utility/Constants.java
+++ ewm-service/src/main/java/ru/practicum/ewm/utility/Constants.java
@@ -4,5 +4,8 @@ import java.time.format.DateTimeFormatter;
 
 public class Constants {
 
-    public static DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private Constants() {
+    }
+
+    public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 }
