d6712b34539fb79a77f5470bc24105d5ca84e91d
Alexander <71982238+AlSl94@users.noreply.github.com>
2022-11-27 21:22:09 +0300

    feat: added @Repository annotation

--- ewm-service/src/main/java/ru/practicum/ewm/event/dao/EventDao.java
+++ ewm-service/src/main/java/ru/practicum/ewm/event/dao/EventDao.java
@@ -3,12 +3,14 @@ package ru.practicum.ewm.event.dao;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.practicum.ewm.event.EventState;
 import ru.practicum.ewm.event.model.Event;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface EventDao extends JpaRepository<Event, Long> {
 
     List<Event> findByInitiatorId(Long initiatorId, Pageable pageable);
--- ewm-service/src/main/java/ru/practicum/ewm/requests/dao/RequestDao.java
+++ ewm-service/src/main/java/ru/practicum/ewm/requests/dao/RequestDao.java
@@ -1,10 +1,12 @@
 package ru.practicum.ewm.requests.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.ewm.requests.model.Request;
 
 import java.util.List;
 
+@Repository
 public interface RequestDao extends JpaRepository<Request, Long> {
 
     List<Request> findAllByRequesterId(Long id);
